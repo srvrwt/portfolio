0304f231152ffd6ebef424017d6897f505e0208c
srvrwt <saurabhrawat2112000@gmail.com>
2025-08-13 09:14:32 +0530

    My portfolio setup with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import HeroSection from "./components/HeroSection.jsx";
 import Work from "./components/work.jsx";
